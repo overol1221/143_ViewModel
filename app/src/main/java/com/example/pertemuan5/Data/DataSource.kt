0d8eaacf51c86f85f076ecbc885b02e002097487
overol1221 <fiqiachmad12@gmail.com>
2023-10-25 19:09:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan5/Data/DataSource.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan5.Data
+
+import com.example.pertemuan5.R
+
+object DataSource {
+    object datasource {
+        val jenis = listOf(
+            R.string.laki,
+            R.string.wanita
+        )
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan5/DataForm.kt
@@ -0,0 +1,3 @@
+package com.example.pertemuan5
+
+data class DataForm( val sex: String = "")
