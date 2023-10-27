6eb52d71c5830004bb216673b0a4132c1aa5dd09
overol1221 <fiqiachmad12@gmail.com>
2023-10-27 14:09:30 +0700

    Make a text to left

--- app/src/main/java/com/example/pertemuan5/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan5/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -124,6 +125,8 @@ fun TampilForm(cobaViewModel: CobaViewModel = viewModel ()) {
         })
     Text(
         text = "Jenis Kelamin",
+        textAlign = TextAlign.Left,
+        modifier = Modifier.fillMaxWidth()
 
         )
     SelectJK(
